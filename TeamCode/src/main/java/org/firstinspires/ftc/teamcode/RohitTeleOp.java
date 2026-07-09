68d3de71051247dcedf26a15409b84bef8ad9058
lilburla <saiavaneeshburla@gmail.com>
2026-07-09 11:30:19 -0500

    Update RohitTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RohitTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RohitTeleOp.java
@@ -1,2 +1,23 @@
-package org.firstinspires.ftc.teamcode;public class RohitTeleOp {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Rohit TeleOp")
+public class RohitTeleOp extends OpMode {
+    /**
+     * This is called when the driver press INIT
+     */
+    @Override
+    public void init() {
+
+    }
+    /**
+     * This is called while OpMode is playing
+     */
+    @Override
+    public void loop() {
+
+    }
+
 }
